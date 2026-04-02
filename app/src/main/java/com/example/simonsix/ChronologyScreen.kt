6efe4f3faf1e75781ff264a3f0036feaec89748c
SilviaToniolo8 <silvia.toniolo.8@studenti.unipd.it>
2026-04-02 15:14:32 +0200

    set up project

---
+++ app/src/main/java/com/example/simonsix/ChronologyScreen.kt
@@ -0,0 +1,9 @@
+package com.example.simonsix
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChronologyScreen()
+{
+
+}
---
+++ app/src/main/java/com/example/simonsix/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.simonsix
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.simonsix.ui.theme.SimonSixTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        
+        setContent {
+            SimonSixTheme {
+                val navController = rememberNavController()
+                
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "game",
+                        modifier = Modifier.padding(paddingValues = innerPadding)
+                    ) {
+                        composable (route="game") {
+                            GameScreen()
+                        }
+
+                        composable (route="chronology") {
+                            ChronologyScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
