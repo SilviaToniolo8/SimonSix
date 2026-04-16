e8ab644d83affae0f19b1ca307b13ecf0494c8f7
SilviaToniolo8 <silvia.toniolo.8@studenti.unipd.it>
2026-04-16 19:33:08 +0200

    update UI Chronology:  truncated long text with ellipsis

--- app/src/main/java/com/example/simonsix/ChronologyScreen.kt
+++ app/src/main/java/com/example/simonsix/ChronologyScreen.kt
@@ -1,7 +1,6 @@
 package com.example.simonsix
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,19 +20,16 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlin.text.iterator
 
 @Composable
-fun ChronologyScreen(prevGames: MutableList<String>, onBackClicked:() -> Unit) {
+fun ChronologyScreen(prevGames: MutableList<String>, onBackClicked:() -> Unit)
+{
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -47,10 +43,10 @@ fun ChronologyScreen(prevGames: MutableList<String>, onBackClicked:() -> Unit) {
             text = stringResource(R.string.previous_games)
         )
 
-        LazyColumn(modifier = Modifier.fillMaxSize().safeDrawingPadding().weight(0.8f).padding(8.dp)) {
+        LazyColumn(modifier = Modifier.fillMaxSize().weight(0.8f).safeDrawingPadding().padding(8.dp)) {
             items(prevGames){ game ->
                 Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-                    Text(modifier = Modifier.weight(0.1f),
+                    Text(modifier = Modifier.width(48.dp),//.weight(0.1f),
                         text = game.filterNot{ it == ',' }.length.toString(),
                         fontSize = 20.sp
                     )
@@ -58,32 +54,22 @@ fun ChronologyScreen(prevGames: MutableList<String>, onBackClicked:() -> Unit) {
                     Text(
                         modifier = Modifier
                             .weight(1f)
-                            .fillMaxWidth()
                             .padding(10.dp),
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
-                        letterSpacing = 5.sp,
                         lineHeight = 35.sp,
-                        text = buildAnnotatedString {
-                            for (c in game) {
-                                when (c) {
-                                    'R' -> withStyle(style = SpanStyle(Color(colorResource(id = R.color.red).toArgb()))) {append("R")}
-                                    'Y' -> withStyle(style = SpanStyle(Color(colorResource(id = R.color.yellow).toArgb()))) {append("Y")}
-                                    'G' -> withStyle(style = SpanStyle(Color(colorResource(id = R.color.green).toArgb()))) {append("G")}
-                                    'C' -> withStyle(style = SpanStyle(Color(colorResource(id = R.color.cyan).toArgb()))) {append("C")}
-                                    'B' -> withStyle(style = SpanStyle(Color(colorResource(id = R.color.blue).toArgb()))) {append("B")}
-                                    'M' -> withStyle(style = SpanStyle(Color(colorResource(id = R.color.magenta).toArgb()))) {append("M")}
-                                    ',' -> withStyle(style = SpanStyle(Color(colorResource(id = R.color.white).toArgb()))) {append(",")}
-                                }
-                            }
-                        }
+                        letterSpacing = 5.sp,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        text=game,
+                        color = chooseColor(prevGames.indexOf(game))
                     )
                 }
             }
         }
 
         Button(
-            modifier = Modifier.height(80.dp).padding(10.dp),
+            modifier = Modifier.height(70.dp).padding(10.dp),
             colors = buttonColors(
                 containerColor = colorResource(id = R.color.blue),
                 contentColor = Color.White
@@ -103,3 +89,17 @@ fun ChronologyScreen(prevGames: MutableList<String>, onBackClicked:() -> Unit) {
     }
 }
 
+@Composable
+private fun chooseColor(index: Int): Color
+{
+    val colors = listOf(
+        colorResource(id = R.color.red),
+        colorResource(id = R.color.yellow),
+        colorResource(id = R.color.green),
+        colorResource(id = R.color.cyan),
+        colorResource(id = R.color.blue),
+        colorResource(id = R.color.magenta)
+    )
+
+    return colors[index % colors.size]
+}
