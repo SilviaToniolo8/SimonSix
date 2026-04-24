37b95983f708eae3446ab3bea0b715c0780ed4e0
SilviaToniolo8 <silvia.toniolo.8@studenti.unipd.it>
2026-04-24 18:36:16 +0200

    added an object for the list of previous games

---
+++ app/src/main/java/com/example/simonsix/GamesData.kt
@@ -0,0 +1,7 @@
+package com.example.simonsix
+
+object GamesData {
+    // previousGames is the list of sequences already played, shared between screens
+    val previousGames = mutableListOf<String>()
+}
+
